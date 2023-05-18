35b6580f80f157d0eaf2ed960700cc7639d06f71
daohtid <133190561+daohtid@users.noreply.github.com>
2023-05-17 22:42:58 -0700

    Update last python.py

--- last python.py
+++ last python.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
 write # function how_many_months( start, rate, saving, target) where:
 #start - the staring balance in your bank account(float)
 #
