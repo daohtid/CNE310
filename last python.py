fd3243890baab025b0be2c31eda7e79f543cb49c
daohtid <133190561+daohtid@users.noreply.github.com>
2023-05-17 22:08:18 -0700

    add python file
    add python fille homework

---
+++ last python.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and setting
+write # function how_many_months( start, rate, saving, target) where:
+#start - the staring balance in your bank account(float)
+#
+#rate- the monthly interest rate (flaot)- like 0.0001 for0.1%
+#
+#saving- the yearly amount saved for retiredment (float)
+#
+#target-the target saving that you like to achieve
+def how_many_months (start, rate, saving , target ):
+balance = start # give us a value to add onto without changing start
+months = 0
+
